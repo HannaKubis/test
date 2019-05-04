aa9dc729bb6a7604b8ee011589421d3b6619fff5
HannaKubis <hanna.kubis@gmail.com>
2019-05-04 20:25:17 +0200

    Added new book

--- src/Library.java
+++ src/Library.java
@@ -4,14 +4,17 @@ public class Library {
      Book book1 = new Book ("Mroczna wieżą III", "Stephen King", "Zbiegniew Królicki", "Albatros", 559, 2017, 9, "miękka");
      Book book2 = new Book("Ketoza. Wejdź na wyzszy poziom", "Dawid Dobrowolski", "brak",
                 "Dobrowolski", 297,2017, 1, "miekka");
-     Book book3= new Book ("Księga dżungli", "Rudyard Kipling", "Franciszek Mirandola", "Belllona", 217, 2015, 0, "twarda");
+     Book book3= new Book ("Księga dżungli", "Rudyard Kipling", "Franciszek Mirandola", "Bellona", 217, 2015, 0, "twarda");
      Book book4 = new Book ("Nie kończąca się historia", "Michael Ende", "Sławomir Błaut", "Siedmioróg", 480, 2001,0, "twarda");
-
+     Book book5 = new Book ("Paleodieta", "Robb Wolf", "Roman Ociepa", " Publicat Wydawnictwo", 319, 2010, 0, "miękka");
         System.out.println("W biblioteece znajdują się następujące książki: ");
+
         book1.printInfo();
         book2.printInfo();
         book3.printInfo();
         book4.printInfo();
+        book5.printInfo();
     }
 
 }
+
